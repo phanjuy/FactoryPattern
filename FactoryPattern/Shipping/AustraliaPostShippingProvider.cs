32d7d7bc0ec1f7fe0993ed03f7516f1381089071
Duy Phan <dpha@netcompany.com>
2021-12-11 21:49:13 +0100

    Implement AustraliaPostShippingProvider

---
+++ FactoryPattern/Shipping/AustraliaPostShippingProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FactoryPattern.Enums;
+using FactoryPattern.Models;
+
+namespace FactoryPattern.Shipping
+{
+    public class AustraliaPostShippingProvider : ShippingProvider
+    {
+        private readonly string _clientId;
+        private readonly string _secret;
+
+        public AustraliaPostShippingProvider(
+            string clientId,
+            string secret,
+            ShippingCostCalculator shippingCostCalculator,
+            CustomsHandlingOptions customsHandlingOptions,
+            InsuranceOptions insuranceOptions)
+        {
+            _clientId = clientId;
+            _secret = secret;
+
+            ShippingCostCalculator = shippingCostCalculator;
+            CustomsHandlingOptions = customsHandlingOptions;
+            InsuranceOptions = insuranceOptions;
+        }
+
+        public override string GenerateShippingLabelFor(Order order)
+        {
+            var shippingId = GetShippingId();
+
+            if (order.Recipient.Country != order.Sender.Country)
+            {
+                throw new NotSupportedException("International shipping not supported");
+            }
+
+            var shippingCost = ShippingCostCalculator.CalculateFor(
+                order.Recipient.Country,
+                order.Sender.Country,
+                order.TotalWeight);
+
+            return $"Shipping Id: {shippingId} {Environment.NewLine}" +
+                   $"To: {order.Recipient.To} {Environment.NewLine}" +
+                   $"Order total: {order.Total} {Environment.NewLine}" +
+                   $"Tax: {CustomsHandlingOptions.TaxOptions} {Environment.NewLine}" +
+                   $"Shipping Cost: {shippingCost}";
+        }
+
+        private static string GetShippingId()
+        {
+            // Invoke API with API Key
+            return $"AUS-{Guid.NewGuid()}";
+        }
+    }
+}
