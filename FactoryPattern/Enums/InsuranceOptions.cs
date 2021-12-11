0280db523a2d7c257836ee4e05a7bc04b7825158
Duy Phan <dpha@netcompany.com>
2021-12-11 16:05:11 +0100

    Add InsuranceOptions

---
+++ FactoryPattern/Enums/InsuranceOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FactoryPattern.Enums
+{
+    public class InsuranceOptions
+    {
+        public bool ProviderHasInsurance { get; set; }
+        public bool ProviderHasExtendedInsurance { get; set; }
+        public bool ProviderRequiresReturnOnDamage { get; set; }
+    }
+}
