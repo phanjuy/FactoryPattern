84a3de56df9ad9804e04e74d9fecfc2bb2aa81f8
Duy Phan <dpha@netcompany.com>
2021-12-11 21:48:39 +0100

    Add ShippingProvider abstract class

---
+++ FactoryPattern/Shipping/ShippingProvider.cs
@@ -0,0 +1,18 @@
+﻿using FactoryPattern.Enums;
+using FactoryPattern.Models;
+
+namespace FactoryPattern.Shipping
+{
+    public abstract class ShippingProvider
+    {
+        public ShippingCostCalculator ShippingCostCalculator { get; protected set; } = default!;
+
+        public CustomsHandlingOptions CustomsHandlingOptions { get; protected set; } = default!;
+
+        public InsuranceOptions InsuranceOptions { get; protected set; } = default!;
+
+        public bool RequireSignature { get; set; }
+
+        public abstract string GenerateShippingLabelFor(Order order);
+    }
+}
