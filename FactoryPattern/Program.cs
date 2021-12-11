1b33e523985b8f4332a3e8ee857f3ae01572300d
Duy Phan <dpha@netcompany.com>
2021-12-11 21:52:01 +0100

    Add Program code to test

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using FactoryPattern.Models;
 
 namespace FactoryPattern
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Create Order
+            Console.Write("Recipient Country: ");
+            var recipientCountry = Console.ReadLine()!.Trim();
+
+            Console.Write("Sender Country: ");
+            var senderCountry = Console.ReadLine()!.Trim();
+
+            Console.Write("Total Order Weight: ");
+            var totalWeight = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+            var order = new Order
+            {
+                Recipient = new Address
+                {
+                    To = "Filip Ekberg",
+                    Country = recipientCountry
+                },
+
+                Sender = new Address
+                {
+                    To = "Santa Claus",
+                    Country = senderCountry
+                },
+
+                TotalWeight = totalWeight
+            };
+
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m), 1);
+            order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m), 1);
+            #endregion
+
+            var cart = new ShoppingCart(order);
+
+            var shippingLabel = cart.FinalizeOrder();
+
+            Console.WriteLine(shippingLabel);
         }
     }
 }
