3de171e5acd62779d82d5d030a60100c27e7c53d
Duy Phan <dpha@netcompany.com>
2021-12-11 15:51:24 +0100

    Add Address model

---
+++ FactoryPattern/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryPattern.Models
+{
+    public class Address
+    {
+        public string To { get; set; } = default!;
+        public string AddressLine1 { get; set; } = default!;
+        public string? AddressLine2 { get; set; }
+        public string PostalCode { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string Country { get; set; } = default!;
+    }
+}
