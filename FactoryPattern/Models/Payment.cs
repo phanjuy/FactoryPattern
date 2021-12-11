5aa6fb1cfcf6d2d07a8fc1a64348d1c128b2a9e7
Duy Phan <dpha@netcompany.com>
2021-12-11 15:49:57 +0100

    Add Payment model

---
+++ FactoryPattern/Models/Payment.cs
@@ -0,0 +1,10 @@
+﻿using FactoryPattern.Enums;
+
+namespace FactoryPattern.Models
+{
+    public class Payment
+    {
+        public decimal Amount { get; set; }
+        public PaymentProvider PaymentProvider { get; set; }
+    }
+}
