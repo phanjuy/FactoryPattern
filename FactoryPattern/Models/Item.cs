e18dd1f9b14f69c7ae313eb29ce5497ded6f48b1
Duy Phan <dpha@netcompany.com>
2021-12-11 15:50:07 +0100

    Add Item model

---
+++ FactoryPattern/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryPattern.Models
+{
+    public class Item
+    {
+        public Item(string id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+    }
+}
